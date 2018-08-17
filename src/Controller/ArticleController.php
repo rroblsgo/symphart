61c2e8f4a19fa8c7da44e8628870413ec86f0969
Ricardo Robles <rroblesgo@gmail.com>
2018-08-17 17:53:23 +0200

    Después del tercer video - Finalizado

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,13 @@
 
   use App\Entity\Article;
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
+  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
   class ArticleController extends Controller {
     /**
@@ -22,16 +26,97 @@
       return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 
+    /**
+     * @Route("/article/new", name="article_new")
+     * @Method({"GET", "POST"}) 
+     */
+    public function new(Request $request)
+    {
+      $article = new Article();
+      $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class, array('attr' => 
+          array('class' => 'form-control')))
+        ->add('body', TextareaType::class, array(
+          'required' => false,
+          'attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Create',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+         ))
+        ->getForm();
+
+      $form->handleRequest($request);
+      if($form->isSubmitted() && $form->isValid()){
+        $article = $form->getData();
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+
+        return $this->redirectToRoute('article_list');
+      }
+
+      return $this->render('articles/new.html.twig', array(
+        'form' => $form->createView()
+      ));
+    }
+
         /**
+     * @Route("/article/edit/{id}", name="article_edit")
+     * @Method({"GET", "POST"}) 
+     */
+    public function edit(Request $request, $id)
+    {
+      $article = new Article();
+      $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+      $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class, array('attr' => 
+          array('class' => 'form-control')))
+        ->add('body', TextareaType::class, array(
+          'required' => false,
+          'attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Update',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+         ))
+        ->getForm();
+
+      $form->handleRequest($request);
+      if($form->isSubmitted() && $form->isValid()){
+        //$article = $form->getData();
+        $em = $this->getDoctrine()->getManager();
+        //$em->persist($article);
+        $em->flush();
+
+        return $this->redirectToRoute('article_list');
+      }
+
+      return $this->render('articles/edit.html.twig', array(
+        'form' => $form->createView()
+      ));
+    }
+
+    /**
      * @Route("/article/{id}", name="article_show")
-     * @Method({"GET"})
      */
     public function show($id) 
     {
-
       $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
       return $this->render('articles/show.html.twig', array('article' => $article));
+    }
+
+    /**
+     * @Route("/article/delete/{id}", name="article_delete")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) 
+    {
+      $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+      $em = $this->getDoctrine()->getManager();
+      $em->remove($article);
+      $em->flush();
+      $response = new Response();
+      $response->send();
     }
 
 
